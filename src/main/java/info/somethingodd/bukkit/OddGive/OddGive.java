0ab806f70e16ed5aa21e2d711e02bff168cab530
petteyg <petteyg359@gmail.com>
2011-08-29 02:54:57 +0000

    changes

--- src/main/java/info/somethingodd/bukkit/OddGive/OddGive.java
+++ src/main/java/info/somethingodd/bukkit/OddGive/OddGive.java
@@ -1,3 +1,16 @@
+/* This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package info.somethingodd.bukkit.OddGive;
 
 import com.nijiko.permissions.PermissionHandler;
@@ -14,6 +27,9 @@ import java.io.*;
 import java.util.*;
 import java.util.logging.Logger;
 
+/**
+ * @author Gordon Pettey (petteyg359@gmail.com)
+ */
 public class OddGive extends JavaPlugin {
     private Logger log = null;
     private PluginDescriptionFile info = null;
@@ -25,7 +41,6 @@ public class OddGive extends JavaPlugin {
     private OddGivePlayerListener OGPL = null;
     protected Map<Player, Map<String, Boolean>> lists = null;
     private final String configurationFile = "plugins" + File.separator + "OddGive.yml";
-    protected OddItem oddItem = null;
 
     private void configure() {
         File configurationFile = new File(this.configurationFile);
@@ -41,7 +56,7 @@ public class OddGive extends JavaPlugin {
         OGCE = null;
         OGPL = null;
         getServer().getPluginManager().
-        log.info(logPrefix + "disabled" );
+        log.info(logPrefix + "disabled");
     }
 
     @Override
@@ -49,16 +64,16 @@ public class OddGive extends JavaPlugin {
         lists = new HashMap<Player, Map<String, Boolean>>();
         OGCE = new OddGiveCommandExecutor(this);
         OGPL = new OddGivePlayerListener(this);
-        //getCommand("give")
         getCommand("give").setExecutor(OGCE);
         getCommand("i").setExecutor(OGCE);
         getCommand("i0").setExecutor(OGCE);
         getCommand("og").setExecutor(OGCE);
         getServer().getPluginManager().registerEvent(Event.Type.PLAYER_LOGIN, OGPL, Event.Priority.Normal, this);
         getServer().getPluginManager().registerEvent(Event.Type.PLAYER_QUIT, OGPL, Event.Priority.Normal, this);
-        oddItem = (OddItem) getServer().getPluginManager().getPlugin("OddItem");
-        Plugin p = getServer().getPluginManager().getPlugin("Permissions");
-        if (p != null) ph = ((Permissions) p).getHandler();
+        try {
+            ph = ((Permissions) getServer().getPluginManager().getPlugin("Permissions")).getHandler();
+        } catch (NullPointerException e) {
+        }
         log.info(logPrefix + info.getVersion() + " enabled");
     }
 
@@ -78,7 +93,7 @@ public class OddGive extends JavaPlugin {
             getServer().getPluginManager().disablePlugin(this);
             return;
         }
-        BufferedReader i = new BufferedReader(new InputStreamReader(getClass().getResourceAsStream("/OddItem.yml")));
+        BufferedReader i = new BufferedReader(new InputStreamReader(getClass().getResourceAsStream("/OddGive.yml")));
         BufferedWriter o = new BufferedWriter(fw);
         try {
             String line = i.readLine();
